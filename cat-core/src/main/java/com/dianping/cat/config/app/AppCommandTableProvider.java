bc13e119b00bdc617907f7579f0b8ec265d9aaab
jialinsun <jialin.sun@dianping.com>
2016-06-17 19:45:02 +0800

    add table provider

--- cat-core/src/main/java/com/dianping/cat/config/app/AppCommandTableProvider.java
+++ cat-core/src/main/java/com/dianping/cat/config/app/AppCommandTableProvider.java
@@ -1,17 +1,30 @@
 package com.dianping.cat.config.app;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Map;
 
 import org.codehaus.plexus.personality.plexus.lifecycle.phase.Initializable;
 import org.codehaus.plexus.personality.plexus.lifecycle.phase.InitializationException;
 import org.unidal.dal.jdbc.QueryEngine;
 import org.unidal.dal.jdbc.mapping.TableProvider;
+import org.unidal.lookup.annotation.Inject;
 import org.unidal.lookup.annotation.Named;
 
+import com.dianping.cat.Cat;
 import com.dianping.cat.app.AppCommandData;
+import com.dianping.cat.command.entity.Command;
+import com.dianping.cat.configuration.mobile.entity.Item;
 
 @Named(type = TableProvider.class, value = AppCommandTableProvider.LOGIC_TABLE_NAME)
 public class AppCommandTableProvider implements TableProvider, Initializable {
+
+	@Inject
+	private AppCommandConfigManager m_appCommandConfigManager;
+
+	@Inject
+	private MobileConfigManager m_mobileConfigManager;
 
 	public final static String LOGIC_TABLE_NAME = "app-command-data";
 
@@ -21,11 +34,32 @@ public class AppCommandTableProvider implements TableProvider, Initializable {
 
 	private String m_dataSourceName = "app";
 
+	private Date m_historyDate;
+
 	@Override
 	public String getDataSourceName(Map<String, Object> hints) {
 		AppCommandData command = (AppCommandData) hints.get(QueryEngine.HINT_DATA_OBJECT);
+		int commandId = command.getCommandId();
+		Date period = command.getPeriod();
 
-		return m_dataSourceName + "_" + command.getCommandId() % 5;
+		if (period.before(m_historyDate)) {
+			return m_dataSourceName + "_" + commandId % 5;
+		} else {
+			Item item = m_mobileConfigManager.queryConstantItem(MobileConstants.SOURCE, 10);
+			String appName = "美团主APP";
+
+			if (item != null) {
+				appName = item.getValue();
+			}
+
+			Command cmd = m_appCommandConfigManager.getRawCommands().get(commandId);
+
+			if (appName.equals(cmd.getNamespace())) {
+				return m_dataSourceName + "_" + (commandId % 5 + 5);
+			} else {
+				return m_dataSourceName + "_" + commandId % 5;
+			}
+		}
 	}
 
 	@Override
@@ -40,6 +74,17 @@ public class AppCommandTableProvider implements TableProvider, Initializable {
 		return m_physicalTableName + "_" + command.getCommandId();
 	}
 
+	@Override
+	public void initialize() throws InitializationException {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+
+		try {
+			m_historyDate = sdf.parse("2016-06-17 00:00");
+		} catch (ParseException e) {
+			Cat.logError(e);
+		}
+	}
+
 	public void setDataSourceName(String dataSourceName) {
 		m_dataSourceName = dataSourceName;
 	}
@@ -47,9 +92,4 @@ public class AppCommandTableProvider implements TableProvider, Initializable {
 	public void setLogicalTableName(String logicalTableName) {
 		m_logicalTableName = logicalTableName;
 	}
-
-	@Override
-	public void initialize() throws InitializationException {
-	}
-
 }
